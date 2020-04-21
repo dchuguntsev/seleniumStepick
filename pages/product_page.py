c664a3f8262aa19dd1ca4c561866b0d446cce135
dchuguntsev <dchuguntsev@htc-cs.ru>
2020-04-21 16:53:00 +0400

    add check price and book name

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,3 +8,17 @@ class productPage(BasePage):
         link = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
         link.click()
         self.solve_quiz_and_get_code()
+
+    def get_price(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ".alertinner > p > strong").text
+
+    def get_book_name(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ".alertinner > strong").text
+
+    def validate_product(self, book, price):
+        bookname = self.browser.find_element(By.CSS_SELECTOR, ".col-sm-6 > h1").text
+        bookprice = self.browser.find_element(By.CSS_SELECTOR, ".col-sm-6 > p").text
+        if bookname == book and bookprice == price:
+            return True
+        else:
+            return False
--- test_product_page.py
+++ test_product_page.py
@@ -2,9 +2,15 @@ from .pages.product_page import productPage
 import time
 
 
+
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019."
     page = productPage(browser, link)
     page.open()
     page.add_to_basket()
-    time.sleep(15)
+    print(page.get_price())
+    print(page.get_book_name())
+    assert page.validate_product(page.get_book_name(), page.get_price())
+    time.sleep(5)
+
+
